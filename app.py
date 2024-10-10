5cbb0e4104277f50f582481537769a38a74386da
ruyteer <ruyteraraujo992@gmail.com>
2024-10-10 09:14:46 -0300

    test

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 st.title("Dashboard Relatório - Empresas")
+
+data = {
+    ano: [2018, 2019, 2020, 2021, 2022],
+    vendas: [100, 150, 230, 350, 563]
+}
+
+df = pd.DataFrame(data)
+
+st.dataframe(df)
