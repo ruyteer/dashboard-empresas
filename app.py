6ad404c465986a8921a98b47ce24de0c3555539a
ruyteer <ruyteraraujo992@gmail.com>
2024-10-10 09:15:49 -0300

    test again

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 st.title("Dashboard Relatório - Empresas")
 
 data = {
-    ano: [2018, 2019, 2020, 2021, 2022],
-    vendas: [100, 150, 230, 350, 563]
+    'ano': [2018, 2019, 2020, 2021, 2022],
+    'vendas': [100, 150, 230, 350, 563]
 }
 
 df = pd.DataFrame(data)
